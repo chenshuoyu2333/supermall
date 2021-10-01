c144c304b2d0d425120c0b5092b9ab4474c5f682
chenshuoyu <1054532174@qq.com>
2021-10-01 18:28:19 +0800

    'second'

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from "@/router";
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import { Swipe, SwipeItem } from 'vant';
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(Swipe).use(SwipeItem).use(ElementPlus).mount('#app')
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="block">
+    <el-carousel height="150px">
+      <el-carousel-item v-for="item in banners" :key="item.arm" >
+        <a :href="item.link">
+          <img :src="item.image" alt="">
+        </a>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+</template>
+<script>
+export default {
+
+  name: "HomeSwiper",
+  props: {
+    banners: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+.block{
+
+  height: 150px
+}
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+  text-align: center;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+img {
+  width: 100%;
+  height: 100%;
+}
+
+
+</style>
